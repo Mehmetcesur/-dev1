5deeaedc866c528d517c61d67a82a0fda34fb19a
mehmetcesur <mehmetceesur@gmail.com>
2023-10-23 23:21:49 +0300

    C# Dersleri 7: Short Veri Tipi

--- ödev1/Program.cs
+++ ödev1/Program.cs
@@ -1,8 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 // Value Types
 //Console.WriteLine("Hello, World!");
+short number3 = 32767;
 int number1 = 2147483647;
 long number2 = 9223372036854775807;
 Console.WriteLine("Number1 is {0}",number1);
 Console.WriteLine("Number1 is {0}", number2);
+Console.WriteLine("Number1 is {0}", number3);
 Console.ReadLine();
