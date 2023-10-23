557d5ca413b316c742006e8e93771fff21a4ad2f
mehmetcesur <mehmetceesur@gmail.com>
2023-10-24 00:57:39 +0300

    C# Dersleri 18: Switch Bloğuyla Çalışmak

--- conditionals/Program.cs
+++ conditionals/Program.cs
@@ -4,6 +4,13 @@ var number = 11;
 if (number==10)
 {
     Console.WriteLine("Number is 10");
+                break;
+            case 20:
+                Console.WriteLine("Number is 20");
+                break;
+            default:
+                Console.WriteLine("Number is not 10 or 20");
+                break;
 }
 else if (number==20)
 {
