65f494caee995c825ee5976d12a5e4d3005d7ccd
mehmetcesur <mehmetceesur@gmail.com>
2023-10-23 23:16:59 +0300

    C# Dersleri 5: Integer Veri Tipi

--- ödev1/Program.cs
+++ ödev1/Program.cs
@@ -1,3 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Value Types
+//Console.WriteLine("Hello, World!");
+int number1 = 2147483647;
+Console.WriteLine("Number1 is {0}",number1);
 Console.ReadLine();
