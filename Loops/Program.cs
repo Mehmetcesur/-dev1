98fb9782a6ebe8da6c7948d790a4fe980e85dee1
mehmetcesur <mehmetceesur@gmail.com>
2023-10-24 19:28:11 +0300

    C# Dersleri 30: For Döngüsü

---
+++ Loops/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 100; i >= 0; i-=2)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("Finished!");
+
+            Console.ReadLine();
+        }
+    }
+}
