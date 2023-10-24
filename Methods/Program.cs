2bbfe3be5e06bf51aaca1ffa643221af0b043d4c
mehmetcesur <mehmetceesur@gmail.com>
2023-10-24 16:26:57 +0300

    C# Dersleri 23: Default Parametreli Metotlarla Çalışmak

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -3,7 +3,7 @@ Add();
 Add();
 Add();
 Add();
-var result = Add2(20, 30); 
+var result = Add2(); 
 Console.WriteLine(result);
 Console.ReadLine();
 
@@ -13,7 +13,7 @@ static void Add()
     Console.WriteLine("Added !!!");
 }
 
-static int Add2(int number1,int number2)
+static int Add2(int number1=20,int number2 = 30)
 {
     var result = number1 + number2;
     return result;
