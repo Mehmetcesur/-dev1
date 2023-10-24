f6b694f7321bcff832293af0b33a27bcb475a6b5
mehmetcesur <mehmetceesur@gmail.com>
2023-10-24 23:53:14 +0300

    C# Dersleri 34: Örnek Uygulama : Asal Sayı

--- Loops/Program.cs
+++ Loops/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Loops
+﻿using System.Runtime.CompilerServices;
+
+namespace Loops
 {
     internal class Program
     {
@@ -7,20 +9,51 @@
             //ForLoop();
             //WhileLoop();
             //DoWhileLoop();
+            //ForeachMethod();
 
-            string[] students = new string[3] { "Engin", "Derin", "Salih" };    
-
-            foreach (var student in students)
+            if (IsPrimeNumber(6))
+                
+                {
+                Console.WriteLine("This is a prime number");               
+                }
+            else 
             
-            {
-            
-            Console.WriteLine(student);
-            
-            }
+               {
+                Console.WriteLine("This is not a prime number");
+               }
 
             Console.ReadLine();
+               }
+                      
+        private static bool IsPrimeNumber(int number) 
+        
+        {
+            bool result = true;
+            for (int i = 2; i < number-1; i++) 
+            
+            {
+                if (number%i==0)
+                {
+                    result = false;
+                    i = number;
+                }
+            
+            }
+            return result;
 
 
+        }
+
+
+        private static void ForeachMethod()
+        {
+            string[] students = new string[3] { "Engin", "Derin", "Salih" };
+
+            foreach (var student in students)
+
+            {
+                Console.WriteLine(student);
+            }
         }
 
         private static void DoWhileLoop()
