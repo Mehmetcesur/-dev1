ace6ba852ba2df30c490b1267fb8c6d8346160dd
mehmetcesur <mehmetceesur@gmail.com>
2023-10-26 19:52:20 +0300

    C# Dersleri 44: Gerçek Hayatta İnterface Kullanımı 2

--- Interfaces/ICustomerDal.cs
+++ Interfaces/ICustomerDal.cs
@@ -57,6 +57,27 @@ namespace Interfaces
 
         }
     }
+
+    class MySqlCustomerDal : ICustomerDal
+    {
+        public void Add()
+        {
+            Console.WriteLine("MySql Added");
+
+        }
+
+        public void Delete()
+        {
+            Console.WriteLine("MySql Deleted");
+
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("MySql Updaded");
+
+        }
+    }
     class CustomerManager
     {
 
--- Interfaces/Program.cs
+++ Interfaces/Program.cs
@@ -5,12 +5,29 @@
         static void Main(string[] args)
         {
             //InterfacesIntro();
-            
-            CustomerManager customerManager = new CustomerManager();
-            customerManager.Add(new OracleCustomerDal());
+            //Demo();
 
+            ICustomerDal[] customerDals = new ICustomerDal[3]
+            {
+                new SqlServerCustomerDal(),
+                new OracleCustomerDal(),
+                new MySqlCustomerDal()
+
+            };
+             foreach (var customerDal in customerDals)
+            {
+                customerDal.Add();
+              
+            }
+            
 
             Console.ReadLine();
+        }
+
+        private static void Demo()
+        {
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add(new OracleCustomerDal());
         }
 
         private static void InterfacesIntro()
