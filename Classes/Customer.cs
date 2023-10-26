e30787efceeb5cd32c25cf1fafde47ab86df89df
mehmetcesur <mehmetceesur@gmail.com>
2023-10-26 04:46:28 +0300

    C# Dersleri 40: Recap Demo Dama Tahtası Yapmak

--- Classes/Customer.cs
+++ Classes/Customer.cs
@@ -13,10 +13,7 @@ namespace Classes
 
         string _firstName;
         public string FirstName
-        {
-            get { return "Mrs." + _firstName; }  
-            set { _firstName = value; }
-        }
+        public string FirstName { get; set; }
         public string LastName { get; set; }
         public string City { get; set; }
 
