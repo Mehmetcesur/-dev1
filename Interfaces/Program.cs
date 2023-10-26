f5669bb441f15ea16bf97d6915052a150018a5a7
mehmetcesur <mehmetceesur@gmail.com>
2023-10-26 17:56:17 +0300

    C# Dersleri 42: İnterfaceleri Neden Kullanıyoruz?

--- Interfaces/Program.cs
+++ Interfaces/Program.cs
@@ -4,33 +4,56 @@
     {
         static void Main(string[] args)
         {
+            PersonManager manager = new PersonManager();
+            manager.Add(new Customer { Id = 1, FirstName = "Engin", LastName = "Demiroğ", Address = "Ankara" });
+
+            Student student = new Student
+            {
+                Id = 1,
+                FirstName="Derin",
+                LastName="Demiroğ",
+                Departmant="Computer Sciences"
+            };
+
+            manager.Add(student);
+
+            Console.ReadLine();
         }
+        
     }
 
     interface IPerson
     {
         int Id { get; set; }
-        int FirstName { get; set; }
+        string FirstName { get; set; }
 
-        int LastName { get; set; }
+        string LastName { get; set; }
     }
 
     class Customer : IPerson
 
     {
         public int Id { get; set; }
-        public int FirstName { get; set; }
-        public int LastName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
         public string Address { get; set; }
     }
 
     class Student : IPerson
     {
         public int Id { get ; set ; }
-        public int FirstName { get; set ; }
-        public int LastName { get ; set ; }
+        public string FirstName { get; set ; }
+        public string LastName { get ; set ; }
         public string Departmant { get; set; } 
     }
+    class PersonManager
+    {
 
+        public void Add(IPerson person)
+        {
+            Console.WriteLine(person.FirstName);
+
+        }
+    }
 
 }
